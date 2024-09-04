eba8583f5ee33162d56d484441ee42a695aa4c44
Grégory Thognard <drangoht@gmail.com>
2024-09-04 10:11:09 +0200

    Add project files.

---
+++ AzureServiceBusPOC/SecretAppsettingReader.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace AzureServicebusPOC
+{
+
+    public class SecretAppsettingReader
+    {
+        public string ReadValue(string keyName)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables();
+            var configurationRoot = builder.Build();
+            return configurationRoot.GetValue<string>(keyName);
+        }
+        public T ReadSection<T>(string sectionName)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables();
+            var configurationRoot = builder.Build();
+
+            return configurationRoot.GetSection(sectionName).Get<T>();
+        }
+    }
+}
